90b17676222f4ccaf5e371bd411399a601ec54f8
Wylie Tech <matthew.t.wylie@googlemail.com>
2018-03-13 13:41:26 +0000

    Update ArraySorterTest.java
    Implemented more of the tests

--- src/test/java/algorithms/ArraySorterTest.java
+++ src/test/java/algorithms/ArraySorterTest.java
@@ -8,7 +8,6 @@ class ArraySorterTest {
 
   @Test
   void canSortArray() {
-
     int[] testData = {7, 1, 3, 2, 6, 9, 4, 8, 5, 10};
     int[] results = ArraySorter.sortArray(testData);
     checkArrayIsSorted(results);
