39cce2e7cd0ffab38922d617792687626857c442
Wylie Tech <matthew.t.wylie@googlemail.com>
2018-03-13 13:41:02 +0000

    Implemented more of the tests

--- src/test/java/algorithms/ListSorterTest.java
+++ src/test/java/algorithms/ListSorterTest.java
@@ -1,5 +1,6 @@
 package algorithms;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Arrays;
@@ -15,14 +16,31 @@ class ListSorterTest {
     checkListIsSorted(results);
   }
 
+  @Test
+  void emptyListIsReturnedUnchanged() {
+    Integer[] testData = new Integer[0];
+    List<Integer> results = ListSorter.sort(Arrays.asList(testData));
+    assertEquals(0, testData.length);
+  }
+
+  @Test
+  void alreadySortedListIsReturnedUnchanged() {
+    Integer[] testData = new Integer[]{7,8,9,10,11};
+    List<Integer> results = ListSorter.sort(Arrays.asList(testData));
+    checkListIsSorted(results);
+
+  }
+
+
+
   private void checkListIsSorted(List<Integer> data) {
     //An array with less that two elements must by definition be sorted
     if (data.size() < 2) {
       return;
     }
 
-    for (int i = 0; i < data.size() - 1; i++) {
-      if (data.get(i + 1) < data.get(i)) {
+    for (int i = 1; i < data.size(); i++) {
+      if (data.get(i-1) > data.get(i)) {
         fail("Array is not sorted - item" + i + 1 + " is not greater or equal to item " + i);
       }
     }
